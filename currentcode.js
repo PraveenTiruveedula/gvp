49d32022d85ab03006c8a47449fe6d063bfbd4dc
Praveen T <61682300+PraveenTiruveedula@users.noreply.github.com>
2025-03-16 12:23:44 +0530

    Update currentcode.js

--- currentcode.js
+++ currentcode.js
@@ -1,62 +1,38 @@
-import React, { useState } from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native';
+import * as React from 'react';
+import { SafeAreaView, ScrollView, Text, Image } from 'react-native';
+import { PaperProvider, Card, Button } from 'react-native-paper';
 
-export default function ListExample() {
-  const [names, setNames] = useState([
-    {
-      id: 0,
-      name: 'Ben',
-    },
-    {
-      id: 1,
-      name: 'Susan',
-    },
-    {
-      id: 2,
-      name: 'Robert',
-    },
-    {
-      id: 3,
-      name: 'Mary',
-    }
-   ]);
-
-  alertItemName = (item) => {
-    alert(item.name +" " +item.id);
-  };
+export default function MovieCard({movieItem}) {
+  const leftComponent = ({ size }: { size: number }) => (
+    <Image
+      resizeMode="cover"
+      style={{ width: size, height: size, borderRadius: size / 2 }}
+      source={{
+        uri: 'https://raw.githubusercontent.com/PraveenTiruveedula/gvp/refs/heads/main/movieLogo.png',
+      }}
+    />
+  );
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        {names.map((item, index) => (
-          <TouchableOpacity
-            key={item.id}
-            style={styles.container}
-            onPress={() => this.alertItemName(item)}>
-            <Text style={styles.text}>{item.name}</Text>
-          </TouchableOpacity>
-        ))}
-      </ScrollView>
-    </View>
+    <ScrollView>
+      <Card style={{ margin: 20 }}>
+        <Card.Title
+          title={movieItem.movieName}
+          subtitle={movieItem.movieType}
+          titleStyle={{ fontSize: 18, fontWeight: 'bold' }}
+          subtitleStyle={{ fontSize: 14 }}
+          left={leftComponent}
+        />
+        <Card.Cover
+          style={{ margin: 1, borderRadius: 10, height:300 }}
+          source={{
+          uri: movieItem.posterImage
+          }}
+        />
+        <Card.Actions>
+          <Button>Book</Button>
+          <Button>Share</Button>
+        </Card.Actions>
+      </Card>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    padding: 14,
-    margin: 2,
-    borderColor: '#2a4914',
-    borderWidth: 1,
-  },
-  text: {
-    color: '#4f603c',
-    fontSize: 14,
-    textAlign: 'left',
-  },
-});
