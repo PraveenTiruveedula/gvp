0dad31a6e9d85e15cd2a227cbeee3faeb0e0eddc
Praveen T <61682300+PraveenTiruveedula@users.noreply.github.com>
2025-03-16 11:53:34 +0530

    Create currentcode.js

---
+++ currentcode.js
@@ -0,0 +1,18 @@
+  const [names, setNames] = useState([
+    {
+      id: 0,
+      name: 'Ben',
+    },
+    {
+      id: 1,
+      name: 'Susan',
+    },
+    {
+      id: 2,
+      name: 'Robert',
+    },
+    {
+      id: 3,
+      name: 'Mary',
+    }
+   ]);
